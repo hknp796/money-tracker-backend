aa75e5bf2f19c0be622dd3607aba09db666403da
hari-iocod <harikrishnan@iocod.com>
2023-07-16 17:56:11 +0530

    api added

---
+++ app/models/Transaction.js
@@ -0,0 +1,14 @@
+// import { model,Schema } from "mongoose";
+const mongoose  =require('mongoose')
+const {Schema,model} = mongoose
+const TransactionSchema = new Schema({
+    name: { type: String, required: true },
+    price:{type:Number ,required:true},
+    description: { type: String, required: true },
+    datetime: { type: String, required: true }
+})
+
+
+ const TransactionModel  = model('Transaction',TransactionSchema)
+
+module.exports =TransactionModel
